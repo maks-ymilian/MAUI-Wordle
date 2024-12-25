f8b00986a7f8cf18e82a4d0ecfcd1eebb9338dcd
Maks <maxiomaxiowski8@gmail.com>
2024-12-25 20:01:53 +0000

    add binding for grid size

--- GamePage.xaml.cs
+++ GamePage.xaml.cs
@@ -1,22 +1,52 @@
+
 namespace Wordle;
 
 public partial class GamePage : ContentPage
 {
+    public static readonly BindableProperty WordSizeProperty =
+            BindableProperty.Create(nameof(WordSize), typeof(int), typeof(GamePage), 5, propertyChanged: OnGridSizeChanged);
+
+    public static readonly BindableProperty RowCountProperty =
+        BindableProperty.Create(nameof(RowCount), typeof(int), typeof(GamePage), 6, propertyChanged: OnGridSizeChanged);
+
+    public int WordSize
+    {
+        get => (int)GetValue(WordSizeProperty);
+        set => SetValue(WordSizeProperty, value);
+    }
+
+    public int RowCount
+    {
+        get => (int)GetValue(RowCountProperty);
+        set => SetValue(RowCountProperty, value);
+    }
+
     public GamePage()
     {
-        int wordSize = 5;
-        int rowCount = 6;
-
         InitializeComponent();
+        BindingContext = this;
 
-        for (int i = 0; i < rowCount; i++)
+        RebuildGrid();
+    }
+
+    private static void OnGridSizeChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        if (bindable is GamePage page)
+            page.RebuildGrid();
+    }
+
+    private void RebuildGrid()
+    {
+        WordleVerticalStack.Clear();
+
+        for (int i = 0; i < RowCount; i++)
         {
             HorizontalStackLayout layout = new()
             {
                 Style = (Style)Resources["WordleVertical"]
             };
 
-            for (int j = 0; j < wordSize; j++)
+            for (int j = 0; j < WordSize; j++)
             {
                 layout.Add(new BoxView()
                 {
