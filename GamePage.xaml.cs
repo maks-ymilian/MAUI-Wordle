2ec98bf94f52ba5cd1a478619fb24befc6e925b3
Maks <maxiomaxiowski8@gmail.com>
2024-12-25 18:45:46 +0000

    dynamically create grid

--- GamePage.xaml.cs
+++ GamePage.xaml.cs
@@ -2,8 +2,29 @@ namespace Wordle;
 
 public partial class GamePage : ContentPage
 {
-	public GamePage()
-	{
-		InitializeComponent();
-	}
+    public GamePage()
+    {
+        int wordSize = 5;
+        int rowCount = 6;
+
+        InitializeComponent();
+
+        for (int i = 0; i < rowCount; i++)
+        {
+            HorizontalStackLayout layout = new()
+            {
+                Style = (Style)Resources["WordleVertical"]
+            };
+
+            for (int j = 0; j < wordSize; j++)
+            {
+                layout.Add(new BoxView()
+                {
+                    Style = (Style)Resources["WordleBox"]
+                });
+            }
+
+            WordleVerticalStack.Add(layout);
+        }
+    }
 }
