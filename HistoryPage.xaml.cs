b4d3e5f01d2c5c2f5a2399b39376d1ed87c3e249
Maks <maxiomaxiowski8@gmail.com>
2024-12-28 18:11:25 +0000

    add scrollview

--- HistoryPage.xaml.cs
+++ HistoryPage.xaml.cs
@@ -2,8 +2,29 @@ namespace Wordle;
 
 public partial class HistoryPage : ContentPage
 {
+	public List<Color> List { get; set; } = new()
+	{
+		Colors.White,
+		Colors.Black,
+		Colors.White,
+		Colors.Black,
+		Colors.White,
+		Colors.Black,
+		Colors.White,
+		Colors.Black,
+		Colors.White,
+		Colors.Black,
+		Colors.White,
+		Colors.Black,
+		Colors.White,
+		Colors.Black,
+		Colors.White,
+		Colors.Black,
+	};
+
 	public HistoryPage()
 	{
 		InitializeComponent();
+		BindingContext = this;
 	}
 }
